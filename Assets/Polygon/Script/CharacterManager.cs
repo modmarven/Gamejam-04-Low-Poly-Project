7ef063c1d08ec406faab4519225f038fed1314fa
modmarven <modmarven@gmail.com>
2024-03-25 07:22:03 -0700

    movement system

--- Assets/Polygon/Script/CharacterManager.cs
+++ Assets/Polygon/Script/CharacterManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Movement inputSystem;
+    private CharacterController characterController;
+    private Animator animator;
+
+    [Header("Movement Setting")]
+    public float currentSpeed = 3.0f;
+    public float walkSpeed = 3.0f;
+    public float turnSmoothVelocity;
+    public float turnSmoothTime = 1.0f;
+
+    [Header("Mouse Look Setting")]
+    public Transform cameraTransform;
+
+    
+    void Awake()
     {
+        inputSystem = new Movement();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        HandlingCharacterMovement();
+        HandlingCharacterAnimation();
+    }
+
+    private void HandlingCharacterMovement()
+    {
+        Vector2 inputVector = inputSystem.Player.Move.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            Vector3 direction = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection * currentSpeed * Time.deltaTime);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandlingCharacterAnimation()
     {
-        
+        Vector2 inputVector = inputSystem.Player.Move.ReadValue<Vector2>();
+        animator.SetFloat("xMove", inputVector.x);
+        animator.SetFloat("yMove", inputVector.y);
+    }
+
+    private void OnEnable()
+    {
+        inputSystem.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputSystem.Enable();
     }
 }
